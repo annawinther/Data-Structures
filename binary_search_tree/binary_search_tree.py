44b50bd75ddc587566421acda11b0a6c6ae8645e
Anna Winther <anski.anna@gmail.com>
2020-02-26 12:35:31 +0000

    Psuedo code and start to code out BST

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,5 +1,5 @@
-import sys
-sys.path.append('../queue_and_stack')
+# import sys
+# sys.path.append('../queue_and_stack')
 from dll_queue import Queue
 from dll_stack import Stack
 
@@ -12,15 +12,61 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        # LEFT CASE
+        # check if our new nodes value is less than the current nodes value
+        if value < self.value:
+            # does it have a child to the left?
+            if not self.left:
+                # place our new node here
+                self.left = BinarySearchTree(value)
+            # otherise 
+            else:
+                # repeat process for the left --> turn around and 
+                self.left.insert(value)
+                
+        # RIGHT CASE
+        # check if the new nodes value is greater than or equal to the current nodes value
+        if value >= self.value:
+            # does it have a child to the right?
+            if not self.right:
+                # place one here
+                self.right = BinarySearchTree(value)
+            # otherwise
+            else:
+                # repeat the process for the right
+
+        # Compare root node
+            # If lesser go to left child
+        # If greater or equal to go to right child
+        # Else try again starting from the child on appropriate
+
+    # if exsists then add to right: greater than or equal to --> add to right 
+    # AND less than -->  add to left
+
+    # minimum value, store values unitl we get to the None, return that value
+
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
+        # BASE CASE 
+
+        # LEFT CASE
+
+        # RIGHT CASE
         pass
 
     # Return the maximum value found in the tree
     def get_max(self):
+        # BASE CASE 
+        # if empty tree
+            # return none
+        
+        # RECURSIVE CASE
+        # if there is no right value
+            # return the root node value
+        # otherwise 
+            # return get max of the 
         pass
 
     # Call the function `cb` on the value of each node
